8770c971e31946f252e29a6287064fd73b44df72
MaksymilianBilski <biger9@o2.pl>
2022-12-29 17:55:52 +0100

    complete

---
+++ src/components/App.jsx
@@ -0,0 +1,88 @@
+import { AddContacts } from './Form/Form';
+import { Section } from './Section/Section';
+import { SearchForm } from './SearchByName/SearchForm';
+import { nanoid } from 'nanoid';
+import { ContactsList } from './ContactsList/ContactsList';
+import React, { Component } from 'react';
+
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: [],
+      filter: '',
+    };
+  }
+
+  componentDidMount() {
+    if (
+      JSON.parse(localStorage.getItem('localUsers')) === undefined ||
+      JSON.parse(localStorage.getItem('localUsers')) === null
+    ) {
+      return;
+    }
+    const localUsers = JSON.parse(localStorage.getItem('localUsers'));
+    this.setState({
+      contacts: localUsers,
+    });
+  }
+  componentDidUpdate() {
+    // add all contacts to local storage
+    localStorage.setItem('localUsers', JSON.stringify(this.state.contacts));
+  }
+  onFormSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const name = form.elements.name;
+    const number = form.elements.number;
+
+    if (
+      this.state.contacts.find(
+        contact =>
+          contact.name === name.value && contact.number === number.value
+      )
+    ) {
+      alert(`${name.value} is already in contacts`);
+      return;
+    }
+
+    const contact = {
+      name: name.value,
+      number: number.value,
+      id: nanoid(),
+    };
+    //add single contact to local storage
+    localStorage.setItem('localContact', JSON.stringify(contact));
+    const localContact = JSON.parse(localStorage.getItem('localContact'));
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, localContact],
+    }));
+    form.reset();
+  };
+
+  onFilterChange = evt => {
+    this.setState({ filter: evt.target.value });
+  };
+
+  onContactDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <Section title="Phonebook">
+          <AddContacts onSubmit={this.onFormSubmit} />
+        </Section>
+        <SearchForm value={this.state.filter} onChange={this.onFilterChange} />
+        <ContactsList
+          bookArray={this.state.contacts}
+          filter={this.state.filter}
+          onDelete={this.onContactDelete}
+        />
+      </div>
+    );
+  }
+}
